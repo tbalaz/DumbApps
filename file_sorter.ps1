3952f692c20c327d57576c81cec596a7ad854fac
Tomislav Balaz <Tomislav.Balaz@omega-software.hr>
2015-02-19 23:07:12 +0100

    File sorter
    Enumerate files by extension in selected folder, then create folder with
    name of extension and move all files with that extension to that folder.

---
+++ file_sorter.ps1
@@ -0,0 +1,34 @@
+﻿$files = Get-ChildItem -Path F:\Downloads -File | Select-Object Extension -Unique
+
+$sort = Get-ChildItem -Path F:\Downloads -File
+
+
+
+
+foreach($i in $files){
+
+    $extname =  $i.Extension.Split('.')[1]
+   
+    New-Item -Type Directory -Path f:\downloads\_$extname"_files" -Force
+
+    $path = Get-ChildItem -Path f:\downloads -Directory
+
+    foreach($s in $sort){
+           
+        if($s.Extension -eq $i.Extension){
+        
+            Move-Item -Path $s.PSPath -Destination f:\_$extname"_files"
+            
+            }     
+        
+        }
+
+    }
+
+
+
+
+
+
+
+
